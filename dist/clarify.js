768d0977cff4033c3d2832b8416ab1cc148d1508
Omid Habibi <omid_h28@hotmail.com>
2017-11-27 19:50:29 +0330

    update dist build

--- dist/clarify.js
+++ dist/clarify.js
@@ -8860,17 +8860,20 @@ return /******/ (function(modules) { // webpackBootstrap
 	    });
 	
 	    this._queue = [];
+	    this._passedHandlerArgs = [];
 	    this._stateContainer = stateContainer;
 	  }
 	
 	  _createClass(CallManager, [{
 	    key: 'schedule',
 	    value: function schedule(routeSchema) {
-	      if (routeSchema instanceof _route2.default === false) {
-	        throw new TypeError('schedule: routeSchema must be an instance of Route');
+	      this._queue.push(routeSchema);
+	
+	      for (var _len = arguments.length, handlerArgs = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+	        handlerArgs[_key - 1] = arguments[_key];
 	      }
 	
-	      this._queue.push(routeSchema);
+	      this._passedHandlerArgs.push([].concat(handlerArgs));
 	    }
 	  }, {
 	    key: 'apply',
@@ -8897,10 +8900,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this._stateContainer.resolve();
 	      }
 	
-	      var nextItem = this._queue[0];
-	      var awaitForIt = nextItem.awaitForHandler;
+	      var nextRoute = this._queue[0];
+	      var awaitForIt = nextRoute.awaitForHandler;
 	      var handlerArguments = [];
-	      var storedDependencies = this._loadDependenciesFor(nextItem);
+	      var storedDependencies = this._loadDependenciesFor(nextRoute);
 	      var _iteratorNormalCompletion = true;
 	      var _didIteratorError = false;
 	      var _iteratorError = undefined;
@@ -8926,31 +8929,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	      }
 	
-	      var result = nextItem.handler.apply(nextItem, handlerArguments);
-	      this._queue.shift();
-	      if (awaitForIt && result instanceof Promise) {
-	        result.then(function (resultAfterPromise) {
-	          return _this2._processNextItem(resultAfterPromise, nextItem.storeResultAs);
-	        });
-	      } else {
-	        return this._processNextItem(result, nextItem.storeResultAs);
-	      }
-	    }
-	  }, {
-	    key: '_loadDependenciesFor',
-	    value: function _loadDependenciesFor(route) {
-	      var storedDependencies = [];
-	      var dependencyKeys = route.dependencies;
+	      var passedHandlerArgs = this._passedHandlerArgs[0];
 	      var _iteratorNormalCompletion2 = true;
 	      var _didIteratorError2 = false;
 	      var _iteratorError2 = undefined;
 	
 	      try {
-	        for (var _iterator2 = dependencyKeys[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-	          var key = _step2.value;
+	        for (var _iterator2 = passedHandlerArgs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+	          var passedArgument = _step2.value;
 	
-	          var dependency = this._stateContainer.fetch(key);
-	          storedDependencies.push(dependency);
+	          handlerArguments.push(passedArgument);
 	        }
 	      } catch (err) {
 	        _didIteratorError2 = true;
@@ -8963,6 +8951,47 @@ return /******/ (function(modules) { // webpackBootstrap
 	        } finally {
 	          if (_didIteratorError2) {
 	            throw _iteratorError2;
+	          }
+	        }
+	      }
+	
+	      var result = nextRoute.handler.apply(nextRoute, handlerArguments);
+	      this._queue.shift();
+	      if (awaitForIt && result instanceof Promise) {
+	        result.then(function (resultAfterPromise) {
+	          return _this2._processNextItem(resultAfterPromise, nextRoute.storeResultAs);
+	        });
+	      } else {
+	        return this._processNextItem(result, nextRoute.storeResultAs);
+	      }
+	    }
+	  }, {
+	    key: '_loadDependenciesFor',
+	    value: function _loadDependenciesFor(route) {
+	      var storedDependencies = [];
+	      var dependencyKeys = route.dependencies;
+	      var _iteratorNormalCompletion3 = true;
+	      var _didIteratorError3 = false;
+	      var _iteratorError3 = undefined;
+	
+	      try {
+	        for (var _iterator3 = dependencyKeys[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+	          var key = _step3.value;
+	
+	          var dependency = this._stateContainer.fetch(key);
+	          storedDependencies.push(dependency);
+	        }
+	      } catch (err) {
+	        _didIteratorError3 = true;
+	        _iteratorError3 = err;
+	      } finally {
+	        try {
+	          if (!_iteratorNormalCompletion3 && _iterator3.return) {
+	            _iterator3.return();
+	          }
+	        } finally {
+	          if (_didIteratorError3) {
+	            throw _iteratorError3;
 	          }
 	        }
 	      }
